45fb252d8c1c473a1d72ebe1ec12ed87ff0888ab
Vishav Singh <vishav_singh@intuit.com>
2014-11-12 00:13:29 -0800

    Improving variable nomenclature

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -21,17 +21,16 @@ object Application extends Controller {
   val requestTokenURL = "https://oauth.intuit.com/oauth/v1/get_request_token";
   val accessTokenURL = "https://oauth.intuit.com/oauth/v1/get_access_token";
   val userAuthURL = "https://appcenter.intuit.com/Connect/Begin";
-  val oauthCallback = "http://localhost:9000/requestToken";
-  
+  val oauthCallback = "http://localhost:9000/oauthCallbackImpl";
+
   //Prod Keys
   val consumerKey = "qyprd65wBgNHyLYdB7CAzT13AeDMJb";
   val consumerSecret = "UpMk1eg0zf3VHhB8q7N0Ni0VSmpTrnfvmrJRGoir";
-  
+
   //Dev Keys
-//   val consumerKey = "qyprdpVqUILJSMKPjqAVA6Rk5gUfxg";
-//  val consumerSecret = "iB1f62NdC2DunObkXSKpDmTuYWZ0J3xUHL8c575n";
- 
-  
+  //   val consumerKey = "qyprdpVqUILJSMKPjqAVA6Rk5gUfxg";
+  //  val consumerSecret = "iB1f62NdC2DunObkXSKpDmTuYWZ0J3xUHL8c575n";
+
   var verifier = "";
   var oauth_token = ""
   var oauth_token_secret = ""
@@ -39,27 +38,38 @@ object Application extends Controller {
   var timestamp = ""
   var accessToken = ""
   var accessSecret = ""
-    var realmId=""
+  var realmId = ""
 
   def index = Action {
 
     val requestToken = getRequestToken
-    Redirect(userAuthURL + "?oauth_token=" + requestToken);
-    //Ok(views.html.index("Your new application is ready."))
+    println("About to call Redirect()")
+    Redirect(userAuthURL + "?oauth_token=" + requestToken); 
   }
 
   def getRequestToken: String =
     {
+      println("Entering getRequestToken")
       var oauth_nonce = UUID.randomUUID();
       var timestamp = System.currentTimeMillis() / 1000;
-      val queryString = "?oauth_callback=http%3A%2F%2Flocalhost%3A9000%2FrequestToken&oauth_consumer_key=qyprd65wBgNHyLYdB7CAzT13AeDMJb&oauth_nonce=" + oauth_nonce + "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=" + timestamp + "&oauth_version=1.0";
-      val signatureBaseString = "GET&https%3A%2F%2Foauth.intuit.com%2Foauth%2Fv1%2Fget_request_token&oauth_callback%3Dhttp%253A%252F%252Flocalhost%253A9000%252FrequestToken%26oauth_consumer_key%3Dqyprd65wBgNHyLYdB7CAzT13AeDMJb%26oauth_nonce%3D" + oauth_nonce + "%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D" + timestamp + "%26oauth_version%3D1.0";
 
+      /**
+       * CONSTRUCTION OF THE SIGNATURE BASE STRING. NOTE THAT THE PARAMTERS ARE ALPHABETICALLY ORDERED
+       */
+      val queryString = "?oauth_callback=http%3A%2F%2Flocalhost%3A9000%2FoauthCallbackImpl&oauth_consumer_key=qyprd65wBgNHyLYdB7CAzT13AeDMJb&oauth_nonce=" + oauth_nonce + "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=" + timestamp + "&oauth_version=1.0";
+      val signatureBaseString = "GET&https%3A%2F%2Foauth.intuit.com%2Foauth%2Fv1%2Fget_request_token&oauth_callback%3Dhttp%253A%252F%252Flocalhost%253A9000%252FoauthCallbackImpl%26oauth_consumer_key%3Dqyprd65wBgNHyLYdB7CAzT13AeDMJb%26oauth_nonce%3D" + oauth_nonce + "%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D" + timestamp + "%26oauth_version%3D1.0";
+
+      /**
+       * SIGNATURE CALCULATION AND GENERATION FOR REQUEST TOKEN CALL
+       * Signing key is "<consumerSecret>&"
+       */
       val secret = new SecretKeySpec((consumerSecret + "&").getBytes, "HmacSHA1")
       val mac = Mac.getInstance("HmacSHA1")
       mac.init(secret)
       val signatureByte: Array[Byte] = mac.doFinal((signatureBaseString).getBytes)
+      //BASE 64 Encode the signature because the result of HMAC-SHA is binary
       val signature = new sun.misc.BASE64Encoder().encode(signatureByte)
+      //Append the signature to the end of the Request Token URL along with the query string
       val futureResult: Future[play.api.libs.ws.Response] = WS.url(requestTokenURL + queryString + "&oauth_signature=" + signature).get;
 
       implicit val timeout = Timeout(15 seconds)
@@ -70,17 +80,28 @@ object Application extends Controller {
       val mapOfResultBody = result.body.split("&").map(_ split "=") collect { case Array(k, v) => (k, v) } toMap
 
       oauth_token_secret = mapOfResultBody("oauth_token_secret").toString()
+
+      println("Quitting getRequestToken")
       mapOfResultBody("oauth_token").toString()
 
     }
 
   def getAccessToken =
     {
+      println("Entering getAccessToken")
       var oauth_nonce = UUID.randomUUID();
       var timestamp = System.currentTimeMillis() / 1000;
+
+      /**
+       * CONSTRUCTION OF THE SIGNATURE BASE STRING. NOTE THAT THE PARAMTERS ARE ALPHABETICALLY ORDERED
+       */
       val queryString = "?oauth_consumer_key=qyprd65wBgNHyLYdB7CAzT13AeDMJb&oauth_nonce=" + oauth_nonce + "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=" + timestamp + "&oauth_token=" + oauth_token + "&oauth_verifier=" + verifier + "&oauth_version=1.0"
       val signatureBaseString = "GET&https%3A%2F%2Foauth.intuit.com%2Foauth%2Fv1%2Fget_access_token&oauth_consumer_key%3Dqyprd65wBgNHyLYdB7CAzT13AeDMJb%26oauth_nonce%3D" + oauth_nonce + "%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D" + timestamp + "%26oauth_token%3D" + oauth_token + "%26oauth_verifier%3D" + verifier + "%26oauth_version%3D1.0"
 
+      /**
+       * SIGNATURE CALCULATION AND GENERATION FOR REQUEST TOKEN CALL
+       * Signing key is "<consumerSecret>&<oauth_token_secret>"
+       */
       val secret = new SecretKeySpec((consumerSecret + "&" + oauth_token_secret).getBytes, "HmacSHA1")
       val mac = Mac.getInstance("HmacSHA1")
       mac.init(secret)
@@ -95,20 +116,19 @@ object Application extends Controller {
 
       accessToken = mapOfResultBody("oauth_token").toString()
       accessSecret = mapOfResultBody("oauth_token_secret").toString()
+       println("getAccessToken response body" + result.body.toString())
+      println("Quitting getRequestToken")
     }
 
-  def requestToken = Action { implicit request =>
-
-    println("Inisde" + request);
-    println("body	" + request.getQueryString("oauth_verifier"));
+  def oauthCallbackImpl = Action { implicit request =>
+    println("Entering oauthCallbackImpl")
     verifier = request.getQueryString("oauth_verifier").getOrElse("")
     oauth_token = request.getQueryString("oauth_token").getOrElse("")
     realmId = request.getQueryString("realmId").getOrElse("")
-     println("realm	" +realmId);
+    println("oauthCallbackImpl response body" + request.body.toString())
+    println("Quitting oauthCallbackImpl")
 
-    //  loginForm.bindFromRequest.fold(
-    //    formWithErrors => BadRequest(html.login(formWithErrors)),
-    //    user => Redirect(routes.Projects.index).withSession("email" -> user._1)
+    //Now use this information to get the Access Token
     getAccessToken
     Ok("")
 
